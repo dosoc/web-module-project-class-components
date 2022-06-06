4b9204785ae57c7747c030f410351c297bd02742
dosoc <18337634+dosoc@users.noreply.github.com~>
2022-06-05 21:52:54 -0500

    added dom elements

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -4,7 +4,20 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+
+        <ul>
+          <li>Drive to Chatanooga</li>
+          <li>Read a book</li>
+          <li>Win the State Open</li>
+        </ul>
+
+        <form>
+          <input/>
+          <button>Add</button>
+        </form>
+
+        <button>Clear</button>
       </div>
     )
   }
